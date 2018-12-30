b3e1c3fcdf26b54a9af3725f32852b36cc67dcf4
Leo B <leo.benkel@gmail.com>
2018-12-29 19:37:40 -0800

    Use Maybe instead of Option

--- src/main/scala/thebrains/youradventure/Adventure/Location.scala
+++ src/main/scala/thebrains/youradventure/Adventure/Location.scala
@@ -1,9 +1,11 @@
 package thebrains.youradventure.Adventure
 
+import scalaz.Maybe
+
 class Location(
   name:           String,
   description:    String,
-  parentLocation: Option[Location]
+  parentLocation: Maybe[Location]
 ) extends Things(name, description)
 
 object Locations {
@@ -12,49 +14,49 @@ object Locations {
       extends Location(
         name = "Menu Interface",
         description = "This is a Menu",
-        None
+        Maybe.empty
       )
 
   case object Void
       extends Location(
         name = "void",
         description = "This is nowhere",
-        None
+        Maybe.empty
       )
 
   case object Multiverse
       extends Location(
         name = "Multiverse",
         description = "This is the multiverse",
-        None
+        Maybe.empty
       )
 
   case object OurUniverse
       extends Location(
         name = "Universe1",
         description = "This is our universe",
-        parentLocation = Some(Multiverse)
+        parentLocation = Maybe.just(Multiverse)
       )
 
   case object MilkyWay
       extends Location(
         name = "MilkyWay",
         description = "The milky way galaxy",
-        parentLocation = Some(OurUniverse)
+        parentLocation = Maybe.just(OurUniverse)
       )
 
   case object SolarSystem
       extends Location(
         name = "Solar System",
         description = "This a system of planet",
-        parentLocation = Some(MilkyWay)
+        parentLocation = Maybe.just(MilkyWay)
       )
 
   case object Earth
       extends Location(
         name = "Earth",
         description = "The blue planet, planet Earth",
-        parentLocation = Some(SolarSystem)
+        parentLocation = Maybe.just(SolarSystem)
       )
 
 }
