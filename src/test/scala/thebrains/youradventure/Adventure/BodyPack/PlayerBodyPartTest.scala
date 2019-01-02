534a12abe548ea23365aafbfead5a4c7ab48dc06
Leo B <leo.benkel@gmail.com>
2019-01-01 19:40:38 -0800

    Clean up test for player body part

--- src/test/scala/thebrains/youradventure/Adventure/BodyPack/PlayerBodyPartTest.scala
+++ src/test/scala/thebrains/youradventure/Adventure/BodyPack/PlayerBodyPartTest.scala
@@ -10,15 +10,23 @@ class PlayerBodyPartTest extends ParentTest {
   "PlayerBodyPart" - {
     val playerBodyPart = FPlayerBodyPart()
 
+    def checkEquipping(
+      bodyPart:  PlayerBodyPart,
+      equipment: Equipment
+    ): PlayerBodyPartEquipped = {
+      assert(bodyPart.canEquip(equipment))
+      val result = unsafeRunToEither(bodyPart.equip(equipment))
+      assert(result.isRight)
+      val newBodyPart: PlayerBodyPartEquipped = result.right.get
+      assertEquals(bodyPart.getBodyPart, newBodyPart.getBodyPart)
+      assertEquals(equipment, newBodyPart.getEquipment)
+      newBodyPart
+    }
+
     "Equipment" - {
       "Should equip with good equipment" in {
         val equipment = FEquipment(bodyPart = playerBodyPart.getBodyPart.just)
-        assert(playerBodyPart.canEquip(equipment))
-        val result = unsafeRunToEither(playerBodyPart.equip(equipment))
-        assert(result.isRight)
-        val newBodyPart = result.right.get
-        assertEquals(playerBodyPart.getBodyPart, newBodyPart.getBodyPart)
-        assertEquals(equipment, newBodyPart.getEquipment)
+        checkEquipping(playerBodyPart, equipment)
       }
 
       "Should fail to equip with bad equipment" in {
@@ -28,23 +36,10 @@ class PlayerBodyPartTest extends ParentTest {
       }
 
       "Can change equipment" in {
-        def checkEquiping(
-          bodyPart:  PlayerBodyPart,
-          equipment: Equipment
-        ): PlayerBodyPartEquipped = {
-          assert(bodyPart.canEquip(equipment))
-          val result = unsafeRunToEither(bodyPart.equip(equipment))
-          assert(result.isRight)
-          val newBodyPart: PlayerBodyPartEquipped = result.right.get
-          assertEquals(bodyPart.getBodyPart, newBodyPart.getBodyPart)
-          assertEquals(equipment, newBodyPart.getEquipment)
-          newBodyPart
-        }
-
         val equipment: Equipment = FEquipment(bodyPart = playerBodyPart.getBodyPart.just)
-        val newBodyPart = checkEquiping(playerBodyPart, equipment)
+        val newBodyPart = checkEquipping(playerBodyPart, equipment)
         val equipment2 = FEquipment(bodyPart = playerBodyPart.getBodyPart.just)
-        checkEquiping(newBodyPart, equipment2)
+        checkEquipping(newBodyPart, equipment2)
 
       }
     }
