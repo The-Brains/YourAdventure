c9f63babeb11994b322f7de41e6922de8e588e79
Leo B <leo.benkel@gmail.com>
2019-01-05 16:31:15 -0800

    Test for bird operator

--- src/test/scala/thebrains/youradventure/Utils/BirdUtils/BirdOperatorTest.scala
+++ src/test/scala/thebrains/youradventure/Utils/BirdUtils/BirdOperatorTest.scala
@@ -6,22 +6,39 @@ class BirdOperatorTest extends ParentTest {
   "Bird pipe" - {
     import BirdOperator._
     "pipe1" - {
-      def get1: Int = 1
 
-      def add2(a: Int): Int = a + 2
+      def add2(a: Int): Int = a + 10
 
-      val expected = 3
-      assertEquals(expected, get1 |> add2)
+      "Should return piped result from method" in {
+        def get1: Int = 1
+        val expected = 11
+        assertEquals(expected, get1 |> add2)
+      }
+
+      "Should return piped result from val" in {
+        val input: Int = 10
+        val expected = 20
+        assertEquals(expected, input |> add2)
+      }
     }
 
     "pipe2" - {
-      def get: (Int, Int) = (1, 2)
       def sum(
         a: Int,
         b: Int
       ): Int = a + b
-      val expected = 3
-      assertEquals(expected, get |> sum)
+
+      "Should return piped result from method" in {
+        def get: (Int, Int) = (1, 2)
+        val expected = 3
+        assertEquals(expected, get |> sum)
+      }
+
+      "Should return piped result from val" in {
+        val input: (Int, Int) = (10, 2)
+        val expected = 12
+        assertEquals(expected, input |> sum)
+      }
     }
   }
 }
