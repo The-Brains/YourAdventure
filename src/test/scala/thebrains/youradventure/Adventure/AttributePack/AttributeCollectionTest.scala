b378a394555a4be5c948f5a442d3e006047e1538
Leo Benkel <lbenkel@demandbase.com>
2019-01-07 14:54:44 -0800

    Covert empty list test

--- src/test/scala/thebrains/youradventure/Adventure/AttributePack/AttributeCollectionTest.scala
+++ src/test/scala/thebrains/youradventure/Adventure/AttributePack/AttributeCollectionTest.scala
@@ -2,6 +2,7 @@ package thebrains.youradventure.Adventure.AttributePack
 
 import scalaz.Maybe
 import thebrains.youradventure.Adventure.TransformationPack._
+import thebrains.youradventure.FactoriesTest.AttributePack.FAttribute
 import thebrains.youradventure.ParentTest
 import thebrains.youradventure.Utils.ToOption._
 
@@ -30,6 +31,27 @@ class AttributeCollectionTest extends ParentTest {
       assertEquals(11.just, newAttributes.getAttributeValue(Attributes.Strength))
       assertEquals(10.just, newAttributes.getAttributeValue(Attributes.Constitution))
       assertEquals(Maybe.Empty(), newAttributes.getAttributeValue(Attributes.Intelligence))
+    }
+
+    "Empty" - {
+      "Should be empty" in {
+        assertEquals(0, AttributeCollection.Empty.length)
+      }
+
+      "Should be empty from within the class" in {
+        class CollectionTest
+            extends AttributeCollection(
+              attributes = Set(
+                FAttribute().toPlayerAttribute(10),
+                FAttribute().toPlayerAttribute(2)
+              )
+            ) {
+          def emptyPublic: AttributeCollection = this.empty
+        }
+
+        val collection: CollectionTest = new CollectionTest()
+        assertEquals(0, collection.emptyPublic.length)
+      }
     }
   }
 }
--- src/test/scala/thebrains/youradventure/Adventure/BodyPack/PlayerBodyCollectionTest.scala
+++ src/test/scala/thebrains/youradventure/Adventure/BodyPack/PlayerBodyCollectionTest.scala
@@ -5,56 +5,85 @@ import thebrains.youradventure.FactoriesTest.BodyPartPack._
 import thebrains.youradventure.FactoriesTest.FEquipment
 import thebrains.youradventure.ParentTest
 import thebrains.youradventure.Utils.ToOption._
+
 class PlayerBodyCollectionTest extends ParentTest {
   "PlayerBodyCollection" - {
-    val bodyPartA = FPlayerBodyPart()
-    val bodyPartB = unsafeRun(FPlayerBodyPartEquipped())
-    val equipmentB = bodyPartB.getEquipment
-    val collection = FPlayerBodyCollection(
-      bodyParts = List[PlayerBodyPart](bodyPartA, bodyPartB).just
-    )
+    "Empty" - {
+      "Should be length 0" in {
+        class CollectionTest
+            extends PlayerBodyCollection(
+              bodyParts = List(
+                FBodyPart().toPlayerBodyPart,
+                FBodyPart().toPlayerBodyPart
+              )
+            ) {
+          def emptyPublic: PlayerBodyCollection = this.empty
+        }
 
-    "Should return right length" in {
-      assertEquals(2, collection.length)
+        val collection: CollectionTest = new CollectionTest()
+        assertEquals(0, collection.emptyPublic.length)
+      }
     }
 
-    "Should equip correct equipment" in {
-      assertEquals(List(equipmentB), collection.equipments)
-      assertEquals(Maybe.empty, collection.getEquipment(bodyPartA.getBodyPart))
-      assertEquals(equipmentB.just, collection.getEquipment(bodyPartB.getBodyPart))
+    "Combine" - {
+      val originLength = 9
+      val collection = FPlayerBodyCollection(lengthBodyParts = originLength.just)
 
-      val equipmentA = FEquipment(bodyPart = bodyPartA.getBodyPart.just)
-      val result = unsafeRunToEither(collection.equip(equipmentA))
-
-      assert(result.isRight)
-
-      val newCollection = result.right.get
-      assertEquals(List(equipmentA, equipmentB), newCollection.equipments)
-      assertEquals(equipmentA.just, newCollection.getEquipment(bodyPartA.getBodyPart))
-      assertEquals(equipmentB.just, newCollection.getEquipment(bodyPartB.getBodyPart))
+      "Should be correct length" in {
+        assertEquals(originLength, collection.length)
+        assertEquals(originLength + 1, (collection ++ FPlayerBodyPart()).length)
+      }
     }
 
-    "Should not equip bad equipment" in {
-      val equipmentA = FEquipment()
-      val result = unsafeRunToEither(collection.equip(equipmentA))
-      assert(result.isLeft)
+    "Not empty" - {
+      val bodyPartA = FPlayerBodyPart()
+      val bodyPartB = unsafeRun(FPlayerBodyPartEquipped())
+      val equipmentB = bodyPartB.getEquipment
+      val collection = FPlayerBodyCollection(
+        bodyParts = List[PlayerBodyPart](bodyPartA, bodyPartB).just
+      )
+
+      "Should return right length" in {
+        assertEquals(2, collection.length)
+      }
+
+      "Should equip correct equipment" in {
+        assertEquals(List(equipmentB), collection.equipments)
+        assertEquals(Maybe.empty, collection.getEquipment(bodyPartA.getBodyPart))
+        assertEquals(equipmentB.just, collection.getEquipment(bodyPartB.getBodyPart))
+
+        val equipmentA = FEquipment(bodyPart = bodyPartA.getBodyPart.just)
+        val result = unsafeRunToEither(collection.equip(equipmentA))
+
+        assert(result.isRight)
+
+        val newCollection = result.right.get
+        assertEquals(List(equipmentA, equipmentB), newCollection.equipments)
+        assertEquals(equipmentA.just, newCollection.getEquipment(bodyPartA.getBodyPart))
+        assertEquals(equipmentB.just, newCollection.getEquipment(bodyPartB.getBodyPart))
+      }
+
+      "Should not equip bad equipment" in {
+        val equipmentA = FEquipment()
+        val result = unsafeRunToEither(collection.equip(equipmentA))
+        assert(result.isLeft)
+      }
+
+      "Should replace equipment" in {
+        assertEquals(List(equipmentB), collection.equipments)
+        assertEquals(Maybe.empty, collection.getEquipment(bodyPartA.getBodyPart))
+        assertEquals(equipmentB.just, collection.getEquipment(bodyPartB.getBodyPart))
+
+        val equipmentBB = FEquipment(bodyPart = bodyPartB.getBodyPart.just)
+        val result = unsafeRunToEither(collection.equip(equipmentBB))
+
+        assert(result.isRight)
+
+        val newCollection = result.right.get
+        assertEquals(List(equipmentBB), newCollection.equipments)
+        assertEquals(Maybe.empty, newCollection.getEquipment(bodyPartA.getBodyPart))
+        assertEquals(equipmentBB.just, newCollection.getEquipment(bodyPartB.getBodyPart))
+      }
     }
-
-    "Should replace equipment" in {
-      assertEquals(List(equipmentB), collection.equipments)
-      assertEquals(Maybe.empty, collection.getEquipment(bodyPartA.getBodyPart))
-      assertEquals(equipmentB.just, collection.getEquipment(bodyPartB.getBodyPart))
-
-      val equipmentBB = FEquipment(bodyPart = bodyPartB.getBodyPart.just)
-      val result = unsafeRunToEither(collection.equip(equipmentBB))
-
-      assert(result.isRight)
-
-      val newCollection = result.right.get
-      assertEquals(List(equipmentBB), newCollection.equipments)
-      assertEquals(Maybe.empty, newCollection.getEquipment(bodyPartA.getBodyPart))
-      assertEquals(equipmentBB.just, newCollection.getEquipment(bodyPartB.getBodyPart))
-    }
-
   }
 }
